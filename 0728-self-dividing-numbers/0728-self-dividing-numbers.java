3e4a232fae40853f573352bcc601146c4a0da47b
FLOW JOURNET.N <flowjournet@gmail.com>
2026-08-23 15:49:37 +0530

    Time: 3 ms (43.37%), Space: 42.5 MB (81.33%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int num = left; num <= right; num++) {
+
+            int temp = num;
+            boolean valid = true;
+
+            while (temp > 0) {
+
+                int digit = temp % 10;
+
+                // Number cannot contain 0
+                if (digit == 0 || num % digit != 0) {
+                    valid = false;
+                    break;
+                }
+
+                temp = temp / 10;
+            }
+
+            if (valid) {
+                result.add(num);
+            }
+        }
+
+        return result;
+    }
+}
