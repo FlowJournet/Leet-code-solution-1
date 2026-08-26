cc945ec1ccd030aded27815e59be99044ff6b8b9
FLOW JOURNET.N <flowjournet@gmail.com>
2026-08-26 20:18:51 +0530

    Time: 3 ms (37%), Space: 43.3 MB (60.85%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] morse = {
+            ".-", "-...", "-.-.", "-..", ".", "..-.",
+            "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--.."
+        };
+
+        HashSet<String> set = new HashSet<>();
+
+        for (String word : words) {
+            String code = "";
+
+            for (char ch : word.toCharArray()) {
+                code += morse[ch - 'a'];
+            }
+
+            set.add(code);
+        }
+
+        return set.size();
+    }
+}
