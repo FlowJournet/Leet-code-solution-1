783a3c6865018170b2e56cad6ee418253bf2e3cc
FLOW JOURNET.N <flowjournet@gmail.com>
2026-09-19 20:36:37 +0530

    Time: 16 ms (86.98%), Space: 48.2 MB (39.65%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int findLHS(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int max = 0;
+
+        for (int num : map.keySet()) {
+
+            if (map.containsKey(num + 1)) {
+                int length = map.get(num) + map.get(num + 1);
+                max = Math.max(max, length);
+            }
+        }
+
+        return max;
+    }
+}
